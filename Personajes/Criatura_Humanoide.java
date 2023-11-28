116f8ce7e5d4333956a20ae317877768db8df6de
Brynt-PN <70989902bpn@gmail.com>
2023-11-28 02:28:16 -0500

    Completando clase Padre, altan funciones

--- Personajes/Criatura_Humanoide.java
+++ Personajes/Criatura_Humanoide.java
@@ -1,6 +1,23 @@
 package Personajes;
 
 public class Criatura_Humanoide {
-
     
+    String name_G;
+    double life_points;
+    boolean is_Equip;
+    double atack_points;
+    String especialidad;
+    boolean is_life;
+
+    public void Atack(Object objetivo){
+
+    }
+
+    public void Danger(double atk_pnt){
+
+    }
+
+    public void Is_a_life(){
+
+    }
 }
